087b5ce9335ec901efc25e7fdb97d7a5309f58b1
zija56 <ciel.skyblue53@gmail.com>
2024-09-28 14:34:06 +0900

    【対応完了】usersテーブルとsubjectsテーブルのリレーションを定義

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -67,7 +67,7 @@ class User extends Authenticatable
     }
 
     public function subjects(){
-        return ;// リレーションの定義
+        return $this->belongsToMany('App\Models\Users\User', 'subject_users', 'user_id' ,'subject_id')->withPivot('id'); // リレーションの定義(中間テーブル「subject_users」を使って、リレーションを定義。)
     }
 
     // いいねしているかどうか
